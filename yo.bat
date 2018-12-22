3520ae5389c046fbc24d03073162cfceb2037989
Greg Trevellick <gtrevellick@googlemail.com>
2018-12-22 21:09:04 +0000

    .

--- yo.bat
+++ yo.bat
@@ -4,18 +4,19 @@ cd C:\temp
 
 setlocal enableDelayedExpansion
 
-rem call npm install -g yo generator-angular-basic
+call npm install -g yo generator-angular-basic
 
-rem call yo angular-basic
+call yo angular-basic
 
-:: set foo="bar"
-:: echo %foo%
+rem set foo="bar"
+rem echo %foo%
 
 rem /F = loop thru files
 rem /b = bare format output of dir
 rem /o-d = sort output of dir, -d means sort by newest date 
 rem /s = trawl sub-directories
 rem /tc = sort by time created
+
 rem for /F %%x IN ('dir /b/o-d/s/tc *.csproj') DO (
 rem	 %%x 
 rem )
@@ -27,16 +28,7 @@ for /f "eol=: delims=" %%F in ('dir /b/o-d/s/tc *.csproj') do (
   set "folder!folderCnt!=%%F"
 )
 
-echo you picked !folder1!
+:: open first entry
+!folder1!
 
-::print menu
-:: for /l %%N in (1 1 %folderCnt%) do echo %%N - !folder%%N!
-:: echo(
-
-:: :get selection
-:: set selection=
-:: set /p "selection=Enter a folder number: "
-:: echo you picked %selection% - !folder%selection%!
-
-
-pause
+rem pause
