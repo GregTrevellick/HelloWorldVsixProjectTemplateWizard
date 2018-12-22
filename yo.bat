0dce502e6a6bdd431af1f7489c639605c265a999
Greg Trevellick <gtrevellick@googlemail.com>
2018-12-22 18:09:29 +0000

    .

---
+++ yo.bat
@@ -0,0 +1,13 @@
+cd C:\temp
+
+call npm install -g yo generator-angular-basic
+
+call yo angular-basic
+
+rem find most recent .csproj file in all top level folders only
+
+rem launch it
+cd a
+a.csproj
+
+
