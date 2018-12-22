6fc42648a8cde5abbf30fba44ef272aaa4b013bd
Greg Trevellick <gtrevellick@googlemail.com>
2018-12-22 21:10:17 +0000

    .

--- yo.bat
+++ yo.bat
@@ -11,18 +11,14 @@ call yo angular-basic
 rem set foo="bar"
 rem echo %foo%
 
+set folderCnt=0
+
+::build "array" of folders
 rem /F = loop thru files
 rem /b = bare format output of dir
 rem /o-d = sort output of dir, -d means sort by newest date 
 rem /s = trawl sub-directories
 rem /tc = sort by time created
-
-rem for /F %%x IN ('dir /b/o-d/s/tc *.csproj') DO (
-rem	 %%x 
-rem )
-
-::build "array" of folders
-set folderCnt=0
 for /f "eol=: delims=" %%F in ('dir /b/o-d/s/tc *.csproj') do (
   set /a folderCnt+=1
   set "folder!folderCnt!=%%F"
