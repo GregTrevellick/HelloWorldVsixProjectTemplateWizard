070a9c2ffc5667300ef1fa2fb606d82e19a82506
Greg Trevellick <gtrevellick@googlemail.com>
2018-12-22 18:28:30 +0000

    .

--- yo.bat
+++ yo.bat
@@ -1,13 +1,22 @@
+echo off
+
 cd C:\temp
 
-call npm install -g yo generator-angular-basic
+rem call npm install -g yo generator-angular-basic
 
-call yo angular-basic
+rem call yo angular-basic
+
+set foo="bar"
+echo %foo%
+
+dir /s/b *.csproj
 
 rem find most recent .csproj file in all top level folders only
+for /R %%x in (*.csproj) do Echo "%%x" 
 
-rem launch it
-cd a
-a.csproj
 
+rem cd a
+rem a.csproj
+
+pause
 
