39fedaf6787e8cadc706eba2087b43071125b747
Greg Trevellick <gtrevellick@googlemail.com>
2018-12-22 20:45:50 +0000

    .

--- yo.bat
+++ yo.bat
@@ -2,9 +2,9 @@ echo off
 
 cd C:\temp
 
-rem call npm install -g yo generator-angular-basic
+call npm install -g yo generator-angular-basic
 
-rem call yo angular-basic
+call yo angular-basic
 
 rem set foo="bar"
 rem echo %foo%
@@ -14,10 +14,9 @@ rem /b = bare format output of dir
 rem /o-d = sort output of dir, -d means sort by newest date 
 rem /s = trawl sub-directories
 rem /tc = sort by time created
-for /F %%x IN ('dir /b/o-d/s/tc *.csproj') DO echo %%x
+for /F %%x IN ('dir /b/o-d/s/tc *.csproj') DO (
+	 %%x 
+)
 
-rem cd a
-rem a.csproj
-
-pause
-
+rem echo hi1
+rem pause
