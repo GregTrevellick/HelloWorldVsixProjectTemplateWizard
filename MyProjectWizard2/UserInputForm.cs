fcde621291b9a144065a7bbfe779b340f8177e8e
Greg Trevellick <gtrevellick@googlemail.com>
2018-12-23 15:52:38 +0000

    .

--- MyProjectWizard2/UserInputForm.cs
+++ MyProjectWizard2/UserInputForm.cs
@@ -12,16 +12,16 @@ namespace MyProjectWizard2
 
         public UserInputForm()
         {
-            Size = new System.Drawing.Size(155, 265);
+            Size = new Size(200, 150);
 
             label1 = new Label();
-            label1.Location = new System.Drawing.Point(10, 15);
-            label1.Text = "Will open command prompt - ok?";
+            label1.Location = new Point(10, 10);
+            label1.Text = "Will open cmd";
             Controls.Add(label1);
 
             button1 = new Button();
-            button1.Location = new System.Drawing.Point(90, 25);
-            button1.Size = new System.Drawing.Size(50, 25);
+            button1.Location = new Point(10, 50);
+            button1.Size = new Size(50, 25);
             button1.Click += button1_Click;
             Controls.Add(button1);
         }
@@ -52,23 +52,13 @@ namespace MyProjectWizard2
             }
         }
 
-        private void InitializeComponent()
-        {
-            label1 = new Label();
-            SuspendLayout();
-
-            label1.AutoSize = true;
-            label1.Location = new Point(2, 97);
-            label1.Name = "label1";
-            label1.Size = new Size(125, 13);
-            label1.TabIndex = 0;
-            label1.Text = "CMD will ask u questions";
-
-            ClientSize = new Size(284, 261);
-            Controls.Add(label1);
-            Name = "UserInputForm";
-            ResumeLayout(false);
-            PerformLayout();
-        }
+        //private void InitializeComponent()
+        //{
+        //    SuspendLayout();
+        //    ClientSize = new Size(284, 261);
+        //    Name = "UserInputForm";
+        //    ResumeLayout(false);
+        //    PerformLayout();
+        //}
     }
 }
