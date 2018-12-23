69b642bf7b5c7cb1bdc1259933da402034559b03
Greg Trevellick <gtrevellick@googlemail.com>
2018-12-23 08:47:32 +0000

    .

--- MyProjectWizard2/UserInputForm.cs
+++ MyProjectWizard2/UserInputForm.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Drawing;
 using System.Diagnostics;
 using System.Windows.Forms;
 
@@ -6,61 +7,39 @@ namespace MyProjectWizard2
 {
     public partial class UserInputForm : Form
     {
-        /////////////////////////////private static string customMessage;
-        /////////////////////////private TextBox textBox1;
         private Label label1;
         private Button button1;
 
         public UserInputForm()
         {
-            this.Size = new System.Drawing.Size(155, 265);
+            Size = new System.Drawing.Size(155, 265);
 
             label1 = new Label();
             label1.Location = new System.Drawing.Point(10, 15);
             label1.Text = "Will open command prompt - ok?";
-            this.Controls.Add(label1);
+            Controls.Add(label1);
 
             button1 = new Button();
             button1.Location = new System.Drawing.Point(90, 25);
             button1.Size = new System.Drawing.Size(50, 25);
             button1.Click += button1_Click;
-            this.Controls.Add(button1);
-
-            ////////////textBox1 = new TextBox();
-            ////////////textBox1.Location = new System.Drawing.Point(10, 25);
-            ////////////textBox1.Size = new System.Drawing.Size(70, 20);
-            ////////////this.Controls.Add(textBox1);
+            Controls.Add(button1);
         }
-
-        //////////////public static string CustomMessage
-        //////////////{
-        //////////////    get
-        //////////////    {
-        //////////////        return customMessage;
-        //////////////    }
-        //////////////    set
-        //////////////    {
-        //////////////        customMessage = value;
-        //////////////    }
-        //////////////}
 
         private void button1_Click(object sender, EventArgs e)
         {
-            InvokeCommand(@"C:\_git\HelloWorldVsixProjectTemplateWizardYeoman\yo.bat");//////////////////////////////////////////////////, false);
-            ///////////////////////////////customMessage = textBox1.Text;
-            this.Close();
+            InvokeCommand(@"C:\_git\HelloWorldVsixProjectTemplateWizardYeoman\yo.bat");
+            Close();
         }
 
-        public static void InvokeCommand(string batchFileToBeOpened)////////////////////////////////////////, bool useShellExecute)
+        public static void InvokeCommand(string batchFileToBeOpened)
         {
             var start = new ProcessStartInfo()
             {
-                ///////////////////////////////////////////////Arguments,
                 CreateNoWindow = false,
                 FileName = batchFileToBeOpened,
-                UseShellExecute = false,//////////////////////////////////////useShellExecute,//i.e. open exe within its working directory
+                UseShellExecute = false,
                 WindowStyle = ProcessWindowStyle.Normal,
-                //////////////////////////////////WorkingDirectory
             };
 
             try
@@ -75,22 +54,22 @@ namespace MyProjectWizard2
 
         private void InitializeComponent()
         {
-            this.label1 = new System.Windows.Forms.Label();
+            this.label1 = new Label();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(2, 97);
+            this.label1.Location = new Point(2, 97);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(125, 13);
+            this.label1.Size = new Size(125, 13);
             this.label1.TabIndex = 0;
             this.label1.Text = "CMD will ask u questions";
             // 
             // UserInputForm
             // 
-            this.ClientSize = new System.Drawing.Size(284, 261);
-            this.Controls.Add(this.label1);
+            this.ClientSize = new Size(284, 261);
+            this.Controls.Add(label1);
             this.Name = "UserInputForm";
             this.ResumeLayout(false);
             this.PerformLayout();
--- MyProjectWizard2/WizardImplementation.cs
+++ MyProjectWizard2/WizardImplementation.cs
@@ -8,8 +8,20 @@ namespace MyProjectWizard2
 {
     public class WizardImplementation : IWizard
     {
-        private UserInputForm userInputForm;
-        ///////////////private string customMessage;
+        ///////////////////////////////////////private UserInputForm userInputForm;
+
+        public void RunStarted(object automationObject, Dictionary<string, string> replacementsDictionary, WizardRunKind runKind, object[] customParams)
+        {
+            try
+            {
+                var userInputForm = new UserInputForm();
+                userInputForm.ShowDialog();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
 
         // This method is called before opening any item that has the OpenInEditor attribute.  
         public void BeforeOpeningFile(ProjectItem projectItem)
@@ -20,35 +32,14 @@ namespace MyProjectWizard2
         {
         }
 
-        // This method is only called for item templates, not for project templates.  
-        public void ProjectItemFinishedGenerating(ProjectItem projectItem)
-        {
-        }
-
         // This method is called after the project is created.  
         public void RunFinished()
         {
         }
 
-        public void RunStarted(object automationObject,
-            Dictionary<string, string> replacementsDictionary,
-            WizardRunKind runKind, object[] customParams)
+        // This method is only called for item templates, not for project templates.  
+        public void ProjectItemFinishedGenerating(ProjectItem projectItem)
         {
-            try
-            {
-                //////////////////////////////// Display a form to the user. The form collects  input for the custom message.  
-                userInputForm = new UserInputForm();
-                userInputForm.ShowDialog();
-
-                //////////////customMessage = UserInputForm.CustomMessage;
-
-                ////////////// Add custom parameters.  
-                //////////////replacementsDictionary.Add("$custommessage$", customMessage);
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show(ex.ToString());
-            }
         }
 
         // This method is only called for item templates, not for project templates.  
